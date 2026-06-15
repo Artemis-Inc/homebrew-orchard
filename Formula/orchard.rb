1579d63ccb3c2c42e80b0d7177563e0c2da14e4b
Thomas Guthrie <officialthomasguthrie@gmail.com>
2026-06-15 14:01:47 +1200

    orchard 3.1.0: sync checksums to published assets, add livecheck

--- Formula/orchard.rb
+++ Formula/orchard.rb
@@ -13,25 +13,31 @@ class Orchard < Formula
   version "3.1.0"
   license "Apache-2.0"
 
+  # Lets `brew livecheck` (and Homebrew's autobump) detect new releases.
+  livecheck do
+    url "https://github.com/Artemis-Inc/Orchard/releases/latest"
+    regex(%r{tag/v?(\d+(?:\.\d+)+)}i)
+  end
+
   on_macos do
     on_arm do
       url "https://github.com/Artemis-Inc/Orchard/releases/download/v3.1.0/orch-3.1.0-aarch64-apple-darwin.tar.gz"
-      sha256 "68bed8490d9764c4ef8629b6390b7d92b95acd480762ad156ed00b33422977cc"
+      sha256 "351269375ac3ea3b8642e2df651261b04d0befe0a1762837fb77d75555305c61"
     end
     on_intel do
       url "https://github.com/Artemis-Inc/Orchard/releases/download/v3.1.0/orch-3.1.0-x86_64-apple-darwin.tar.gz"
-      sha256 "15dc6facde8eadf920a2a12b0e53879e084b1e65d18474df04a71e06f909bcca"
+      sha256 "65fdffb0d736918db1c397cfffbebf5e2805c5792de6d0c0753fe2df52497280"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/Artemis-Inc/Orchard/releases/download/v3.1.0/orch-3.1.0-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "a8464c08f323c5210c68e5fcfa9038b67533fd37b650c23419162ac173c311ed"
+      sha256 "9c459c9262cded001aef20af2f040a57fea0009c4999c3c484af2070404834c3"
     end
     on_intel do
       url "https://github.com/Artemis-Inc/Orchard/releases/download/v3.1.0/orch-3.1.0-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "1aa872f04204b5bacda6686cf5ddee3e7861199ef6e827d4e746e1f79bc1841a"
+      sha256 "7f6467068ff8304e120f2400bf22d0783058a89938ec5d9aa89d3a86cae0a7e5"
     end
   end
 
